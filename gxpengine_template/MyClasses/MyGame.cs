b1674e9719faf1d02280774a19d7262274032bcc
Birka <birkavidit@gmail.com>
2024-02-12 15:51:14 +0100

    the original template

---
+++ gxpengine_template/MyClasses/MyGame.cs
@@ -0,0 +1,17 @@
+﻿using GXPEngine;
+
+
+namespace gxpengine_template.MyClasses
+{
+    public class MyGame : Game
+    {
+        public MyGame() : base(600,600,false)
+        {
+        }
+        static void Main()
+        {
+            new MyGame().Start();
+        }
+
+    }
+}
